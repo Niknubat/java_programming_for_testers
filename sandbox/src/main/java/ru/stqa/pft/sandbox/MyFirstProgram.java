8ee0a38dd2ff568192158a6e1cb9a3eaf4488182
sa.tabunkin <tsagrom@gmail.com>
2022-10-20 11:40:16 +0300

    Изменения в настройках и 9 урок - Выражения и операции

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,26 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello, world!");
+		System.out.println("Hello, world!");		//	Hello, world!
+
+		System.out.println(2 + 2);		//	4
+		System.out.println(2 * 2);		//	4
+		System.out.println(2 / 2);		//	1
+		System.out.println(2 - 2);		//	0
+
+		System.out.println(1 / 2);		//	0
+		System.out.println(1.0 / 2);		//	0.5
+		System.out.println(1 / 2.0);		//	0.5
+		System.out.println(1.0 / 2.0);		//	0.5
+
+		System.out.println("2" + "2");		//	22
+		System.out.println("2" + 2);		//	22
+		System.out.println(2 + "2");		//	22
+
+		System.out.println(2 + 2 * 2);		//	6
+		System.out.println((2 + 2) * 2);		//	8
+
+		System.out.println("2 + 2 = " + 2 + 2);		//	2 + 2 = 22
+		System.out.println("2 + 2 = " + (2 + 2));		//	2 + 2 = 4
 	}
 }

800ebfa45f4fbfa4f58ab5fbf80614a0d72a8cfd
sa.tabunkin <tsagrom@gmail.com>
2022-10-25 23:21:23 +0300

    Создан общий базовый класс HelperBase, для классов-помощников

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -4,50 +4,42 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-public class GroupHelper {
-
-    WebDriver wd;
+public class GroupHelper extends HelperBase {
 
     public GroupHelper(WebDriver wd) {
-        this.wd = wd;
+        super(wd);
 
     }
 
     public void returnToGroupPage() {
-      wd.findElement(By.linkText("group page")).click();
+        click(By.linkText("group page"));
     }
 
     public void submitGroupCreation() {
-      wd.findElement(By.name("submit")).click();
+        click(By.name("submit"));
     }
 
     public void fillGroupForm(GroupData groupData) {
-      wd.findElement(By.name("group_name")).click();
-      wd.findElement(By.name("group_name")).clear();
-      wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
-      // Данным способом(заком строка) выполнила IDE что тоже работает + в классе тоже отличия
+        type(By.name("group_name"), groupData.getName());
+        // Данным способом(заком строка) выполнила IDE что тоже работает + в классе тоже отличия
       // wd.findElement(By.name("group_name")).sendKeys(groupData.name());
-      wd.findElement(By.name("group_header")).click();
-      wd.findElement(By.name("group_header")).clear();
-      wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
-      // Данным способом(заком строка) выполнила IDE что тоже работает + в классе тоже отличия
+        type(By.name("group_header"), groupData.getHeader());
+        // Данным способом(заком строка) выполнила IDE что тоже работает + в классе тоже отличия
       // wd.findElement(By.name("group_header")).sendKeys(groupData.header());
-      wd.findElement(By.name("group_footer")).click();
-      wd.findElement(By.name("group_footer")).clear();
-      wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
-      // Данным способом(заком строка) выполнила IDE что тоже работает + в классе тоже отличия
+        type(By.name("group_footer"), groupData.getFooter());
+        // Данным способом(заком строка) выполнила IDE что тоже работает + в классе тоже отличия
       // wd.findElement(By.name("group_footer")).sendKeys(groupData.footer());
     }
 
     public void initGroupCrreation() {
-      wd.findElement(By.name("new")).click();
+        click(By.name("new"));
     }
 
     public void deleteSelectedGroups() {
-      wd.findElement(By.name("delete")).click();
+        click(By.name("delete"));
     }
 
     public void selectGroup() {
-      wd.findElement(By.name("selected[]")).click();
+        click(By.name("selected[]"));
     }
 }

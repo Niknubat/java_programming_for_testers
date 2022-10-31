c63f7e8076daa6f7990946c48524e83d90b7e0b4
sa.tabunkin <tsagrom@gmail.com>
2022-10-31 14:49:16 +0300

    Поиск максимального элемента в коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -8,22 +8,28 @@ import java.util.Objects;
 // Так используется в курсе(более расширенно)
 // объект с 3-мя атрибутами, есть конструктор и три метода возвращающие эти атрибуты
 public class GroupData {
-    private final String id;
+    private int id;
     private final String name;
     private final String header;
     private final String footer;
 
     public GroupData (String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
-    public GroupData (String id, String name, String header, String footer) {
+    public GroupData (int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
         this.footer = footer;
+    }
+
+    public int getId() { return id; }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() { return name; }
@@ -43,15 +49,11 @@ public class GroupData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(id, groupData.id) && Objects.equals(name, groupData.name);
+        return id == groupData.id && Objects.equals(name, groupData.name);
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(id, name);
-    }
-
-    public String getId() {
-        return id;
     }
 }

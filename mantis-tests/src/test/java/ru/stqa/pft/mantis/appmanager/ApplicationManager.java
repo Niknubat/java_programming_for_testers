0d5caacdd3b8d73db342e4ddb3a7de286d02e041
sa.tabunkin <tsagrom@gmail.com>
2022-11-11 12:35:23 +0300

    (Урок-79) Ленивая инициализация помощников - Реализована ленивая инициализация помощников и драйвера браузера

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -13,9 +13,10 @@ import java.util.Properties;
 
 public class ApplicationManager {
     private final Properties properties;
-    WebDriver wd;
+    private WebDriver wd;
 
     private String browser;
+    private RegistrationHelper registrationHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -25,21 +26,12 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-        if (browser.equals("ChromeDriver")) {
-            wd = new ChromeDriver();
-        } else if (browser.equals("FirefoxDriver")) {
-            wd = new FirefoxDriver();
-        } else if (browser.equals("InternetExplorerDriver")) {
-            wd = new InternetExplorerDriver();
-        }
-        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        wd.get(properties.getProperty("web.baseUrl"));
     }
 
     public void stop() {
-//        Thread.sleep(2);
-        wd.quit();
+        if (wd != null) {
+            wd.quit();
+        }
     }
 
     public HttpSession newSession() {
@@ -48,5 +40,28 @@ public class ApplicationManager {
 
     public String getProperty(String key) {
         return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) { // обеспечивает отсутствие повторонй инициализации
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public WebDriver getDriver() { // метод ленивой инициализации браузера.
+        // драйвер будет инициализ. в тот момент, когда к нему кто-то обратится
+        if (wd == null) {
+            if (browser.equals("ChromeDriver")) {
+                wd = new ChromeDriver();
+            } else if (browser.equals("FirefoxDriver")) {
+                wd = new FirefoxDriver();
+            } else if (browser.equals("InternetExplorerDriver")) {
+                wd = new InternetExplorerDriver();
+            }
+            wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
     }
 }
